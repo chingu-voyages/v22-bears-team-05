d3e30fe8b1f04b433dc68cbf5e87a3ff37316974
AnimeshKC <animesh@ualberta.ca>
2020-08-10 15:17:37 -0600

    frontend base code

---
+++ client/pages/index.js
@@ -0,0 +1,25 @@
+import App from "../components/App"
+//import { initializeApollo } from "../lib/apolloClient"
+
+const IndexPage = () => <App>Boilerplate Code</App>
+
+/*
+fill out getStaticProps properly once the graphQL stuff is figured out
+
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  await apolloClient.query({
+    query: ALL_POSTS_QUERY,
+    variables: allPostsQueryVars,
+  })
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    revalidate: 1,
+  }
+}
+*/
+export default IndexPage
