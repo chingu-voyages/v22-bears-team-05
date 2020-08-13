bbd5a034f1d63899bd804062b4e61e6d743a96c4
Babak Chehraz <babak.chehraz@gmail.com>
2020-08-12 18:08:28 -0700

    [refactor] reformatted App component, added prop-types

--- client/components/App.js
+++ client/components/App.js
@@ -1,3 +1,12 @@
-export default function App({ children }) {
-  return <main>{children}</main>
-}
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = ({children}) => {
+  return <main>{children}</main>;
+};
+
+App.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default App;
