654ad958e864d15e9c270bf7ece6f8c2123530c8
TomLikeABomb <t.depasquale@hotmail.com>
2020-08-14 16:01:14 -0400

    Remove console logs

--- server/graphql/resolvers/goals.js
+++ server/graphql/resolvers/goals.js
@@ -3,10 +3,8 @@ const Goal = require("../../models/Goal");
 module.exports = {
   Query: {
     async getAllGoals(_, __, context) {
-      console.log(context.req.isAuth, typeof context.req.isAuth);
       if (context.req.isAuth === false) throw new Error("not authenticated");
       try {
-        console.log(context.req.userId);
         const goals = await Goal.find({ user: context.req.userId });
         return goals;
       } catch (err) {
