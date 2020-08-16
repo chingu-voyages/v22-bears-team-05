73ebfcc7964a256c7143f1f8b297f5b04b9e7751
Babak Chehraz <babak.chehraz@gmail.com>
2020-08-16 16:56:56 -0700

    [Refactor + Fix] Filter now correctly filters array

--- server/graphql/resolvers/tasks.js
+++ server/graphql/resolvers/tasks.js
@@ -3,54 +3,62 @@ const Goal = require("../../models/Goal")
 module.exports = {
   Mutation: {
     async createTask(_, { taskName, goalId }, context) {
-      if (context.req.isAuth === false) throw new Error("not authenticated")
+      if (!context.req.isAuth) throw new Error("not authenticated")
+
       try {
         const parentGoal = await Goal.findById(goalId)
-        if (parentGoal) {
-          const newTask = await Task.create({
-            parent: goalId,
-            user: context.req.userId,
-            name: taskName,
-          })
-          parentGoal.tasks.push(newTask._id)
-          await parentGoal.save()
-          return newTask
-        } else throw new Error("the specified goalId is not valid")
+
+        if (!parentGoal) {
+          throw new Error("Cannot find a goal by that ID")
+        }
+
+        const newTask = await Task.create({
+          parent: goalId,
+          name: taskName,
+        })
+        parentGoal.tasks.push(newTask._id)
+        await parentGoal.save()
+
+        return newTask
       } catch (err) {
         throw new Error(err)
       }
     },
     async deleteTask(_, { taskId }, context) {
-      if (context.req.isAuth === false) throw new Error("not authenticated")
+      if (!context.req.isAuth) throw new Error("not authenticated")
+      
       try {
         const currentTask = await Task.findById(taskId).populate("parent")
-        if (currentTask) {
-          const parentGoal = currentTask.parent
-          if (parentGoal) {
-            parentGoal.tasks = parentGoal.tasks.filter((id) => id !== taskId)
-            await parentGoal.save()
-          }
-          await currentTask.delete()
-          return parentGoal
-        } else {
-          throw new Error("That task does not exist")
+        if (!currentTask) {
+          throw new Error("Cannot find a task by that ID")
         }
+        
+        const parentGoal = currentTask.parent
+        if (parentGoal) {
+          parentGoal.tasks = parentGoal.tasks.filter((id) => id !== taskId)
+          await parentGoal.save()
+        }
+        await currentTask.delete()
+
+        return parentGoal
       } catch (err) {
         throw new Error(err)
       }
     },
     async completeTask(_, { taskId }, context) {
-      if (context.req.isAuth === false) throw new Error("not authenticated")
+      if (!context.req.isAuth) throw new Error("not authenticated")
+      
       try {
         const currentTask = await Task.findById(taskId).populate("parent")
         if (!currentTask)
           throw new Error("The specified Task Id does not exist")
-        /* deleted tasks persist for some reason
 
-        if (currentTask.subtasks.length > 0)
+        if (currentTask.subtasks.length > 0) {
           throw new Error(
             "Task cannot be completed while it still contains subtasks"
-          ) */
+          )
+        }
+
         const parentGoal = currentTask.parent
         if (parentGoal) {
           parentGoal.totalCompletedSubtasks +=
@@ -59,6 +67,7 @@ module.exports = {
           await parentGoal.save()
         }
         await currentTask.delete()
+
         return parentGoal
       } catch (err) {
         throw new Error(err)
