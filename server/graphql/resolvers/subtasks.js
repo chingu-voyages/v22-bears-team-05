008ac885ce02b3e73000707a3b311637d9d3cfb9
Babak Chehraz <babak.chehraz@gmail.com>
2020-08-16 17:12:52 -0700

    [Refactor] Restructured some logic

--- server/graphql/resolvers/subtasks.js
+++ server/graphql/resolvers/subtasks.js
@@ -3,40 +3,43 @@ const Subtask = require("../../models/Subtask")
 module.exports = {
   Mutation: {
     async createSubtask(_, { subtaskName, taskId }, context) {
-      if (context.req.isAuth === false) throw new Error("not authenticated")
+      if (!context.req.isAuth) throw new Error("not authenticated")
+
       try {
         const parentTask = await Task.findById(taskId)
-        if (parentTask) {
-          console.log(parentTask)
-          const newSubtask = await Subtask.create({
-            parent: parentTask._id,
-            name: subtaskName,
-          })
-          parentTask.subtasks.push(newSubtask._id)
-          await parentTask.save()
-          return newSubtask
-        } else {
-          throw new Error("That taskId is not valid")
+
+        if (!parentTask) {
+          throw new Error("Cannot find a Task by that ID");
         }
+
+        const newSubtask = await Subtask.create({
+          parent: parentTask._id,
+          name: subtaskName,
+        })
+        parentTask.subtasks.push(newSubtask._id)
+        await parentTask.save()
+
+        return newSubtask
       } catch (err) {
         throw new Error(err)
       }
     },
     async deleteSubtask(_, { subtaskId }, context) {
-      if (context.req.isAuth === false) throw new Error("not authenticated")
+      if (!context.req.isAuth) throw new Error("not authenticated")
+
       try {
         const currentSubtask = await Subtask.findById(subtaskId).populate(
           "parent"
         )
-        if (!currentSubtask) throw new Error("That subtask does not exist")
+        if (!currentSubtask) throw new Error("Cannot find subtask by that ID")
         
         const parentTask = currentSubtask.parent
+
         if (parentTask) {
-          parentTask.subtasks = parentTask.subtasks.filter(
-            (id) => id === subtaskId
-          )
+          parentTask.subtasks = parentTask.subtasks.filter((id) => id === subtaskId)
           await parentTask.save()
         }
+        
         await currentSubtask.delete()
         return parentTask
       } catch (err) {
@@ -44,23 +47,22 @@ module.exports = {
       }
     },
     async completeSubtask(_, { subtaskId }, context) {
-      if (context.req.isAuth === false) throw new Error("not authenticated")
+      if (!context.req.isAuth) throw new Error("not authenticated")
+
       try {
-        const currentSubtask = await Subtask.findById(subtaskId).populate(
-          "parent"
-        )
-        if (!currentSubtask)
-          throw new Error("A subtask with the specified id does not exist")
+        const currentSubtask = await Subtask.findById(subtaskId).populate("parent")
+
+        if (!currentSubtask) throw new Error("Cannot find subtask by that ID")
+
         const parentTask = currentSubtask.parent
         if (parentTask) {
           //add to count and remove the task from the list
           parentTask.totalCompletedSubtasks += 1
-          parentTask.subtasks = parentTask.subtasks.filter(
-            (id) => id === subtaskId
-          )
+          parentTask.subtasks = parentTask.subtasks.filter((id) => id === subtaskId)
           await parentTask.save()
         }
         await currentSubtask.delete()
+
         return parentTask
       } catch (err) {
         throw new Error(err)
