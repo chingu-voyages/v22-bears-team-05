23663759d72e8f950f94abf1dd9160beabffbd20
root <root@LAPTOP-01SLOJBG.localdomain>
2020-08-29 08:02:37 -0600

    cleanup users resolver - no more jwt code

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -1,23 +1,32 @@
 const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
 const { UserInputError } = require("apollo-server")
 
 const User = require("../../models/User")
 const { validateRegisterInput } = require("./../../utils/validateRegisterInput")
-const SECRET_KEY = process.env.SECRET_KEY
-function generateToken(user) {
-  return jwt.sign(
-    {
-      id: user.id,
-      email: user.email,
-    },
-    SECRET_KEY,
-    { expiresIn: "100d" }
-  )
-}
 
 module.exports = {
   Query: {
+    async userList() {
+      try {
+        const userData = await User.find()
+        const userList = userData.map((element) => {
+          return { email: element.email }
+        })
+        return userList
+      } catch (err) {
+        throw new Error(err)
+      }
+    },
+    async me(_, __, { req }) {
+      if (!req.session.userId) {
+        return null;
+      }
+      const user = await User.findById(req.session.userId);
+      return user;
+    }
+  },
+
+  Mutation: {
     async login(_, { email, password }, context) {
       const errorMessage = "The username or password is invalid" //security best practice to not give too much information
       const errors = {}
@@ -32,31 +41,14 @@ module.exports = {
         errors.general = errorMessage
         throw new UserInputError(errorMessage, { errors })
       }
-      const token = generateToken(user)
-
       // save userId to session!
       context.req.session.userId = user._id;
 
       return {
         id: user._id,
-        token,
         email,
       }
     },
-    async userList() {
-      try {
-        const userData = await User.find()
-        const userList = userData.map((element) => {
-          return { email: element.email }
-        })
-        return userList
-      } catch (err) {
-        throw new Error(err)
-      }
-    },
-  },
-
-  Mutation: {
     async register(_, { email, password, confirmPassword }, context) {
       // Validate user data
       const { valid, errors } = validateRegisterInput(
@@ -69,13 +61,13 @@ module.exports = {
       }
       const user = await User.findOne({ email })
       if (user) {
-        throw new UserInputError("Could not register user", {
+        throw new UserInputError("Email in use", {
           errors: {
-            registration: "Could not register user",
+            registration: "Email in use",
           },
         })
       }
-      // hash password and create an auth token
+      // hash password
       password = await bcrypt.hash(password, 12)
 
       const newUser = new User({
@@ -86,13 +78,10 @@ module.exports = {
 
       const res = await newUser.save()
 
-      const token = generateToken(res)
-
       context.req.session.userId = res._id;
 
       return {
         id: res._id,
-        token,
         email,
       }
     },
