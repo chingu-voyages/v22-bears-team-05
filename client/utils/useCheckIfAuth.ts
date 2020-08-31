228c44f73dcabdeb1075eb6488413b465e089c46
TomLikeABomb <t.depasquale@hotmail.com>
2020-08-31 02:42:18 -0400

    Check if auth only runs once now

--- client/utils/useCheckIfAuth.ts
+++ client/utils/useCheckIfAuth.ts
@@ -1,19 +1,25 @@
 import { useApolloClient } from '@apollo/client';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const useCheckIfAuthLogic = (error: Error | null) => {
   const router = useRouter();
   const client = useApolloClient();
 
-  const clientSideAndNotLoggedIn =
-    error &&
-    error.message === 'not authenticated' &&
-    typeof window !== 'undefined';
+  useEffect(() => {
+    if (error === null) return;
+    console.log('checking auth');
 
-  if (clientSideAndNotLoggedIn) {
-    client.clearStore();
-    router.replace('/login');
-  }
+    const clientSideAndNotLoggedIn =
+      error &&
+      error.message === 'not authenticated' &&
+      typeof window !== 'undefined';
+
+    if (clientSideAndNotLoggedIn) {
+      client.clearStore();
+      router.replace('/login');
+    }
+  }, [client, router, error]);
 };
 
 export const useCheckIfAuth = (): ((error: Error | null) => void) =>
