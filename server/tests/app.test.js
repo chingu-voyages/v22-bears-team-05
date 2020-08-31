2c713df1b8784d12f6dad15d627e653952327eb9
AnimeshKC <animesh@ualberta.ca>
2020-08-31 13:47:51 -0600

    finish register error tests

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -28,14 +28,56 @@ function getTestClientFunctions() {
     return { query, mutate, setOptions }
 }
 test("noUser", async () => {
-    const { query, setOptions } = getTestClientFunctions()
-    // setOptions(generateUIdRequest())
+    const { query } = getTestClientFunctions()
     const meResponse = await query(`{me {email id createdDate}}`)
     expect(meResponse.data.me).toBeNull()
 })
-// test("invalidRegisterEmail", async () => {
 
-// })
+//testing a standalone register error
+test("invalidRegisterEmail", async () => {
+    const { mutate } = getTestClientFunctions()
+    const registerResponse = await mutate(registerTemplate, { variables: { email: "invalidEmail.com", password: "abCd1!", confirmPassword: "abCd1!" } })
+    const errorsObject = registerResponse.errors[0].extensions.errors
+    expect(errorsObject).toHaveProperty("email")
+    expect(errorsObject).not.toHaveProperty("password")
+    expect(errorsObject).not.toHaveProperty("confirmPassword")
+})
+
+test("invalidRegisterPassword", async () => {
+    const { mutate } = getTestClientFunctions()
+    const registerResponse = await mutate(registerTemplate, { variables: { email: "a@gmail.com", password: "abc", confirmPassword: "abc" } })
+    const errorsObject = registerResponse.errors[0].extensions.errors
+    expect(errorsObject).toHaveProperty("password")
+    expect(errorsObject).not.toHaveProperty("confirmPassword")
+    expect(errorsObject).not.toHaveProperty("email")
+})
+
+test("invalidRegisterConfirm", async () => {
+    const { mutate } = getTestClientFunctions()
+    const registerResponse = await mutate(registerTemplate, { variables: { email: "a@gmail.com", password: "abcD1!", confirmPassword: "abcD2!" } })
+    const errorsObject = registerResponse.errors[0].extensions.errors
+    expect(errorsObject).toHaveProperty("confirmPassword")
+    expect(errorsObject).not.toHaveProperty("password")
+    expect(errorsObject).not.toHaveProperty("email")
+})
+
+//testing a standalone register error
+test("invalidRegisterEmail", async () => {
+    const { mutate } = getTestClientFunctions()
+    const registerResponse = await mutate(registerTemplate, { variables: { email: "invalidEmail.com", password: "abCd1!", confirmPassword: "abCd1!" } })
+    const errorsObject = registerResponse.errors[0].extensions.errors
+    expect(errorsObject).toHaveProperty("email")
+})
+
+//testing all register errors together
+test("invalidRegisterConstraints", async () => {
+    const { mutate } = getTestClientFunctions()
+    const registerResponse = await mutate(registerTemplate, { variables: { email: "invalidEmail.com", password: "abc!", confirmPassword: "el" } })
+    const errorsObject = registerResponse.errors[0].extensions.errors
+    expect(errorsObject).toHaveProperty("email")
+    expect(errorsObject).toHaveProperty("password")
+    expect(errorsObject).toHaveProperty("confirmPassword")
+})
 
 test("resolversIntegration", async () => {
     const { query, mutate, setOptions } = getTestClientFunctions()
