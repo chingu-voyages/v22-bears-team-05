daf3b0f76985662667aa305afaadae2e0576452f
AnimeshKC <animesh@ualberta.ca>
2020-08-31 11:10:41 -0600

    reduced code duplication

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -8,27 +8,7 @@ beforeAll(async () => await dbHandler.connect())
 
 afterAll(async () => await dbHandler.closeDatabase())
 
-function generateUIdRequest(userId = null) {
-    return { request: { session: { userId } } }
-}
-
-function getTestClientFunctions() {
-    const apolloServer = createTestServer()
-    return createTestClient({
-        apolloServer,
-    })
-}
-test("noUser", async () => {
-    const { query, setOptions } = getTestClientFunctions()
-    setOptions(generateUIdRequest())
-    const meResponse = await query(`{me {email id createdDate}}`)
-    expect(meResponse.data.me).toBeNull()
-})
-
-test("resolversIntegration", async () => {
-    const { query, mutate, setOptions } = getTestClientFunctions()
-    setOptions(generateUIdRequest())
-    const registerTemplate = `
+const registerTemplate = `
     mutation Register($email: String!, $password: String!, $confirmPassword: String!){
         register(email: $email, password: $password, confirmPassword: $confirmPassword){
             email
@@ -36,9 +16,34 @@ test("resolversIntegration", async () => {
         }
     }
 `
-    const registerResponse = await mutate(registerTemplate, { variables: { email: "abCd@gmail.com", password: "abCd1!", confirmPassword: "abCd1!" } });
+
+function generateUIdRequest(userId = null) {
+    return { request: { session: { userId } } }
+}
+
+function getTestClientFunctions() {
+    const apolloServer = createTestServer()
+    const { query, mutate, setOptions } = createTestClient({ apolloServer, })
+    setOptions(generateUIdRequest())
+    return { query, mutate, setOptions }
+}
+test("noUser", async () => {
+    const { query, setOptions } = getTestClientFunctions()
+    // setOptions(generateUIdRequest())
+    const meResponse = await query(`{me {email id createdDate}}`)
+    expect(meResponse.data.me).toBeNull()
+})
+// test("invalidRegisterEmail", async () => {
+
+// })
+
+test("resolversIntegration", async () => {
+    const { query, mutate, setOptions } = getTestClientFunctions()
+    // setOptions(generateUIdRequest())
+
+    const registerResponse = await mutate(registerTemplate, { variables: { email: "abcd@gmail.com", password: "abCd1!", confirmPassword: "abCd1!" } });
     let userId = registerResponse.data.register.id
-    expect(registerResponse.data.register.email).toEqual("abCd@gmail.com");
+    expect(registerResponse.data.register.email).toEqual("abcd@gmail.com");
     setOptions(generateUIdRequest(userId)) //put the user to the context
 
     const meResponse = await query(`{me {email id createdDate}}`)
