e91a1cc099abef342f4754111f054f1ee8967ca3
Babak Chehraz <babak.chehraz@gmail.com>
2020-09-01 00:45:17 -0700

    [client mutation added: logout]

--- client/utils/graphql/mutation/index.js
+++ client/utils/graphql/mutation/index.js
@@ -2,10 +2,12 @@ import REGISTER_MUTATION from './register';
 import LOGIN_MUTATION from './login';
 import DELETE_GOAL_MUTATION from './deleteGoal';
 import CREATE_GOAL_MUTATION from './createGoal';
+import LOGOUT_MUTATION from './logout';
 
 export {
   LOGIN_MUTATION,
   REGISTER_MUTATION,
   DELETE_GOAL_MUTATION,
   CREATE_GOAL_MUTATION,
+  LOGOUT_MUTATION,
 };
