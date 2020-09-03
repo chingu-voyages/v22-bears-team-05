4509eb989d686502a34c8493fea9eb55f2e63c28
Babak Chehraz <babak.chehraz@gmail.com>
2020-09-03 16:02:37 -0700

    [Removed redirect from login/register to /home when logged in]

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,37 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
-import { useQuery } from '@apollo/client';
 
 import { withApollo } from '../utils/withApollo';
 import App from '../components/App';
 import Register from '../components/Forms/Register';
-import { ME_QUERY } from '../utils/graphql/query';
 
-const RegisterPage = () => {
-  const { data } = useQuery(ME_QUERY);
+const RegisterPage = () => (
+  <App>
+    <Head>
+      <title>Goal Tracker | Register</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
 
-  if (data?.me) {
-    window.location.pathname = '/home';
-  }
-
-  return (
-    <App>
-      <Head>
-        <title>Goal Tracker | Register</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-
-      <h1>Register</h1>
-      <Register />
-      <p className="center">
-        {'Already a member? '}
-        <Link href="/login">
-          <a href="/login">Sign In!</a>
-        </Link>
-      </p>
-    </App>
-  );
-};
+    <h1>Register</h1>
+    <Register />
+    <p className="center">
+      {'Already a member? '}
+      <Link href="/login">
+        <a href="/login">Sign In!</a>
+      </Link>
+    </p>
+  </App>
+);
 
 export default withApollo({ ssr: false })(RegisterPage);
