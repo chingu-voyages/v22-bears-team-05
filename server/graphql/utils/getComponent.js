1f710b168c3367a2abb21659d94b5f0a8150a735
AnimeshKC <animesh@ualberta.ca>
2020-09-12 10:18:57 -0600

    cleaning up console logs

--- server/graphql/utils/getComponent.js
+++ server/graphql/utils/getComponent.js
@@ -2,14 +2,14 @@ const Goal = require("../../models/Goal");
 const Task = require("../../models/Task");
 const Subtask = require("../../models/Subtask");
 
-async function getComponent(componentType, componentId, userId) {
+async function getComponent(componentType, componentId) {
     let component
     if (componentType.trim().toLowerCase() === "goal") {
-        component = await Goal.findOne({ _id: componentId, user: userId })
+        component = await Goal.findById(componentId)
     } else if (componentType.trim().toLowerCase() === "task") {
-        component = await Task.findOne({ _id: componentId, user: userId })
+        component = await Task.findById(componentId)
     } else if (componentType.trim().toLowerCase() === "subtask") {
-        component = await Subtask.findOne({ _id: componentId, user: userId })
+        component = await Subtask.findById(componentId)
     }
     return component
 }
