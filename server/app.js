6d6c9b54b43d851803f48ec81068c1f667c2c354
Babak Chehraz <babak.chehraz@gmail.com>
2020-09-13 15:57:21 -0700

    Set sameSite back to 'none' for production

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ async function startApp() {
         cookie: {
           maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
           httpOnly: true,
-          sameSite: __prod__ ? "lax" : "lax", // csrf
+          sameSite: __prod__ ? "none" : "lax", // csrf
           secure: __prod__, // cookie only works in https
           domain: undefined,
         },
