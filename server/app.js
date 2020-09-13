0df793c4486ba81dcddefaa1d5179cae6a322727
Babak Chehraz <babak.chehraz@gmail.com>
2020-09-13 15:21:38 -0700

    Domain set to undefined again for cookie in prod

--- server/app.js
+++ server/app.js
@@ -55,8 +55,7 @@ async function startApp() {
           httpOnly: true,
           sameSite: __prod__ ? "lax" : "lax", // csrf
           secure: __prod__, // cookie only works in https
-          domain: __prod__ ? ".herokuapp.com" : undefined,
-          path: '/graphql',
+          domain: undefined,
         },
         saveUninitialized: false,
         secret: process.env.SESSION_SECRET,
